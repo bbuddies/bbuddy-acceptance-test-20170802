aead5652c10a1c19360ffc0eb391e3d77a3d11fd
Allen Wang <s972024@gmail.com>
2017-08-03 10:27:38 +0800

    add budget: add acceptance test if we have duplicate data

--- features/budgets.feature
+++ features/budgets.feature
@@ -6,3 +6,12 @@ Feature: Budget
     Then list budgets as below
       | month   | amount |
       | 2017-08 | 1000   |
+
+  Scenario: When we add a budget but there was already one in the same year/month, we should update the data
+    Given the original data is as below
+      | month   | amount |
+      | 2017-08 | 1000   |
+    When add a budget of month "2017-08" with amount 3000
+    Then list budgets is as below
+      | month   | amount |
+      | 2017-08 | 3000   |
--- features/step_definitions/budgets_steps.rb
+++ features/step_definitions/budgets_steps.rb
@@ -12,3 +12,26 @@ Then(/^list budgets as below$/) do |table|
   wait_for_text budget['month']
   wait_for_text budget['amount']
 end
+
+Given(/^the original data is as below$/) do |table|
+    touch('Budgets')
+    touch('Add')
+    budget = table.hashes[0]
+    clear_then_enter_text('month', budget['month'])
+    clear_then_enter_text('amount', budget['amount'])
+    touch('Save')
+
+    wait_for_text 'Dashboard'
+   
+    touch('Budgets')
+    touch('Add')
+    budget = table.hashes[0]
+    clear_then_enter_text('month', budget['month'])
+    clear_then_enter_text('amount', budget['amount'])
+    touch('Save')
+   
+    wait_for_text 'Dashboard'
+    budget = table.hashes[0]
+    wait_for_text budget['month']
+    wait_for_text budget['amount']
+end
